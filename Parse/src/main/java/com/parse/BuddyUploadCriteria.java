8284aa487d9c15ee1d79ba18e7c2b12a1147bc93
Lama Chandrasena <lama@buddy.com>
2017-08-01 11:20:30 +0930

    added the null check to updateInitialPowerStatus

--- Parse/src/main/java/com/parse/BuddyUploadCriteria.java
+++ Parse/src/main/java/com/parse/BuddyUploadCriteria.java
@@ -89,13 +89,19 @@ class BuddyUploadCriteria {
     public void updateInitialPowerStatus(Context context) {
         IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
         Intent batteryStatus = context.registerReceiver(null, ifilter);
-        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
-        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING || status == BatteryManager.BATTERY_STATUS_FULL;
-        if (isCharging) {
-            setPowerStatus(BuddyPowerConnectionStatus.Connected);
+
+        if (batteryStatus == null) {
+            setPowerStatus(BuddyPowerConnectionStatus.Disconnected);
         }
         else {
-            setPowerStatus(BuddyPowerConnectionStatus.Disconnected);
+            int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+            boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING || status == BatteryManager.BATTERY_STATUS_FULL;
+            if (isCharging) {
+                setPowerStatus(BuddyPowerConnectionStatus.Connected);
+            }
+            else {
+                setPowerStatus(BuddyPowerConnectionStatus.Disconnected);
+            }
         }
     }
 
